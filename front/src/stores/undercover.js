d1b1f3b7356bb113b88c4d92b54e433da2e3dd93
LalbaAnthony <anthony.lalba@limayrac.fr>
2024-09-14 22:20:04 +0200

    Start implement undercover logic

--- front/src/stores/undercover.js
+++ front/src/stores/undercover.js
@@ -2,7 +2,24 @@ import { defineStore } from 'pinia'
 
 export const useUndercoverStore = defineStore('undercover', {
   state: () => ({
+    persist: true,
     words: [],
+    roles: [],
+
+    // Game state
+    players: [],
+    currentPlayer: null,
+    currentRound: 1,
+    isGameStarted: false,
+    isGameFinished: false,
+    numberOfPlayers: 3,
+
+    // Game settings
+    numberOfCivilians: 3,
+    numberOfUndercovers: 1,
+    numberOfMrWhite: 1,
+    undercoversWord: '',
+    civilianWord: '',
   }),
 
   actions: {
@@ -24,9 +41,48 @@ export const useUndercoverStore = defineStore('undercover', {
       )
     },
 
-    pickRandomWord () {
+    addAPlayer (name) {
+      this.players.push({ name: name, role: null })
+    },
+
+    async assignRoles () {
+      if (this.players.length !== this.numberOfPlayers) {
+        console.error('Number of players does not match the number of players selected')
+        return
+      }
+
+      if (this.numberOfPlayers < 3) {
+        console.error('Not enough players')
+        return
+      }
+
+      if (this.numberOfPlayers > 20) {
+        console.error('Too many players')
+        return
+      }
+
+      if (this.numberOfCivilians + this.numberOfUndercovers + this.numberOfMrWhite !== this.numberOfPlayers) {
+        console.error('Number of roles does not match the number of players')
+        return
+      }
+
+      // ...
+    },
+
+    async startGame () {
+      this.isGameStarted = true
+      this.currentPlayer = this.players[0]
+    },
+
+    pickRandomWordDuo () {
       const randomIndex = Math.floor(Math.random() * this.words.length)
       return this.words[randomIndex]
+    },
+
+    assignateWords () {
+      const words = this.pickRandomWordDuo()
+      this.undercoversWord = words[0]
+      this.civilianWord = words[1]
     },
   },
 })
