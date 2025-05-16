ff3995b0b206a7352f32120a8c68e7c80e135096
LalbaAnthony <anthonylalaba24@gmail.com>
2025-05-17 01:08:17 +0200

    Improve notif

--- front/src/helpers/notif.js
+++ front/src/helpers/notif.js
@@ -1,24 +1,50 @@
-import { useToast } from 'vue-toastification'; 
-import { isMobile } from '@/helpers/helpers.js'
+import { useToast } from 'vue-toastification';
 
 const toast = useToast();
 
-export function notify(message, type = 'info') {
-    toast[type](message, {
-        position: isMobile() ? 'top-right' : 'bottom-right',
-        timeout: 6000,
+const MAX_NOTIFICATIONS = 2;
+
+let activeNotifications = 0;
+
+function notify(message, type = 'info') {
+    if (!['info', 'success', 'error', 'warning'].includes(type)) {
+        type = 'info';
+        console.error('Invalid type for notification. Defaulting to info.');
+    }
+
+    if (activeNotifications >= MAX_NOTIFICATIONS) {
+        console.warn('Max notifications reached. Ignoring new toast.');
+        return;
+    }
+
+    activeNotifications++;
+
+    const toastId = toast?.[type](message, {
+        position: 'top-right',
+        timeout: 4000,
         closeOnClick: true,
         pauseOnHover: true,
         draggable: true,
-        draggablePercent: 0.6,
+        draggablePercent: 0.3,
         showCloseButtonOnHover: false,
         hideProgressBar: true,
         closeButton: 'button',
-        icon: true,
-        rtl: false
+        icon: false,
+        rtl: false,
+        onClose: () => {
+            activeNotifications--;
+        }
     });
+
+    return toastId;
 }
 
-export function clearNotify() {
+function clear() {
     toast.clear();
+    activeNotifications = 0;
 }
+
+export const notif = {
+    notify,
+    clear
+};
