c44f1f990c12fa176a63831a1291b03bea9bcc23
Anthony LALBA <a.lalba@agoravita.com>
2025-05-20 17:32:32 +0200

    Smol reworks

--- front/src/components/FooterComponent.vue
+++ front/src/components/FooterComponent.vue
@@ -1,10 +1,12 @@
 <template>
   <footer>
-    <p class="text-center text-light-gray text-sm p-4">{{ VITE_APP_NAME }} <br>par {{ VITE_APP_AUTHOR_NAME }}</p>
+    <p class="text-center text-light-gray text-sm p-2">{{ VITE_APP_NAME }} par {{ VITE_APP_AUTHOR_NAME }}<br>
+      Version: {{ VITE_APP_VERSION }}</p>
   </footer>
 </template>
 
 <script setup>
 import { VITE_APP_NAME } from '@/config';
 import { VITE_APP_AUTHOR_NAME } from '@/config';
+import { VITE_APP_VERSION } from '@/config';
 </script>
