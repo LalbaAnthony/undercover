3f4bb730932322ad6c68159b20b85474a3a10ec9
LalbaAnthony <anthonylalaba24@gmail.com>
2025-08-23 14:54:06 +0200

    Addd preloader

--- front/src/App.vue
+++ front/src/App.vue
@@ -12,13 +12,21 @@
 import Header from '@/components/HeaderComponent.vue'
 import Footer from '@/components/FooterComponent.vue'
 import { RouterView} from 'vue-router'
-import { onMounted } from 'vue'
+import { nextTick, onMounted } from 'vue'
 import { useUndercoverStore } from './stores/undercover'
 
 const undercoverStore = useUndercoverStore()
 
-onMounted(() => {
+onMounted(async () => {
   undercoverStore.fetchEverything(false)
+
+  await nextTick();
+
+  // Remove pre-loader from index.html
+  const preLoaderContainer = document.getElementById('pre-loader-container')
+  if (preLoaderContainer) {
+    preLoaderContainer.remove()
+  }
 })
 
 </script>
